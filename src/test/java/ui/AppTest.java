4635149373d616a70fe8840f487a375ccfe89766
kumarQA <sunilkumar_qa@outlook.com>
2020-02-17 20:07:13 +0530

    first commit

---
+++ src/test/java/ui/AppTest.java
@@ -0,0 +1,29 @@
+package ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	public static WebDriver driver;
+
+	@Test
+	public void launchBrowser() {
+		System.out.println(">>> Launching CHROME browser >>>");
+		System.setProperty("webdriver.chrome.driver", "//home/home/Downloads/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://elementary.io/");
+		System.out.println("Chrome lauched successsfully");
+		driver.quit();
+	}
+
+}
